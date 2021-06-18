b3b093950ac77bbde1c42169791efdcafa6274f6
EdaDuan <308506994@qq.com>
2021-06-18 18:09:12 +0800

    "feat: 全选，todoList移动，代码未优化，暂时提交"

--- src/js/init.js
+++ src/js/init.js
@@ -8,16 +8,27 @@ const init = (tasks) => {
     checkBox.setAttribute("id", item.taskId);
     checkBox.setAttribute("type", "checkbox");
     checkBox.setAttribute("value", item.taskName);
+    let label = document.createElement("label");
+    label.setAttribute("for", item.taskId);
     let span = document.createElement("span");
     span.innerText = item.taskName;
+    let delBtn = document.createElement("button");
+    delBtn.innerHTML = "删除";
+    let editorBtn = document.createElement("button");
+    editorBtn.innerHTML = "编辑";
     li.appendChild(checkBox);
+    li.appendChild(label);
     li.appendChild(span);
+    li.appendChild(delBtn);
+    li.appendChild(editorBtn);
     if (item.status) {
       checkBox.setAttribute("name", "todoList");
+      li.classList.add("con-todo-li");
       todoUlItem.appendChild(li);
     } else {
       checkBox.checked = true;
       checkBox.setAttribute("name", "doneList");
+      li.classList.add("con-done-li");
       doneUlItem.appendChild(li);
     }
   });
--- src/js/util/checkBox.js
+++ src/js/util/checkBox.js
@@ -1,25 +1,55 @@
 const checkBox = () => {
+  let todoUlItem = document.getElementsByClassName("con-todo-ul")[0];
+  let doneUlItem = document.getElementsByClassName("con-done-ul")[0];
+  console.log("doneUlItem: ", doneUlItem);
+  let conTodoLi = document.querySelectorAll(".con-todo-li");
+  console.log("conTodoLi: ", conTodoLi);
+  let conDoneLi = document.querySelectorAll(".con-done-li");
   // 今日待办中的todoList
   document.getElementById("btnTodoOperate").addEventListener("click", () => {
-    var arr = new Array();
-    var items = document.getElementsByName("todoList");
-    for (let i = 0; i < items.length; i++) {
-      if (items[i].checked) {
-        arr.push(items[i].value);
-      }
+    // 创建文档片段
+    let fragment = document.createDocumentFragment();
+    for (let i = 0; i < conTodoLi.length; i++) {
+      conTodoLi[i].children[0].checked = true;
+      conTodoLi[i].children[0].name = "doneList";
+      conTodoLi[i].classList.remove("con-todo-li");
+      conTodoLi[i].classList.add("con-done-li");
+      fragment.appendChild(conTodoLi[i]);
     }
-    alert("选择的个数为：" + arr.length);
+    doneUlItem.appendChild(fragment);
   });
+
   // 今日待办中的doneList
   document.getElementById("btnDoneOperate").addEventListener("click", () => {
-    var arr = new Array();
-    var items = document.getElementsByName("doneList");
-    for (let i = 0; i < items.length; i++) {
-      if (items[i].checked) {
-        arr.push(items[i].value);
-      }
+    let fragment = document.createDocumentFragment();
+    for (let i = 0; i < conDoneLi.length; i++) {
+      conDoneLi[i].children[0].checked = false;
+      conDoneLi[i].children[0].name = "todoList";
+      conDoneLi[i].classList.remove("con-done-li");
+      conDoneLi[i].classList.add("con-todo-li");
+      fragment.appendChild(conDoneLi[i]);
     }
-    alert("选择的个数为：" + arr.length);
+    todoUlItem.appendChild(fragment);
   });
+  todoUlItem.addEventListener("click", (e) => {
+    if (e.target.type === "checkbox") {
+      console.log(
+        "🚀 ~ file: checkBox.js ~ line 27 ~ document.getElementsByClassName ~ e",
+        e.path[1]
+      );
+      e.target.checked = true;
+      e.target.name = "doneList";
+      e.path[1].classList.remove("con-todo-li");
+      e.path[1].classList.add("con-done-li");
+      doneUlItem.appendChild(e.path[1]);
+    }
+  });
+  // function cancelChooseColor( clickId ){
+  //   //选中点击的元素
+  //   $(clickId)[0].checked=true;
+  //   //获取选中元素的color值
+  //   // var color =$(clickId).val();
+  //   // $('#color-input-diy-value').val(color);
+  // }
 };
 export default checkBox;
